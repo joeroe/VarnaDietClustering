da121209dc87adb6d01ee7857d064f623f5f3331
Joe Roe <git@joeroe.io>
2024-01-18 09:34:59 +0100

    Add preliminary HDBSCAN analysis
    But a proper go of it requires https://github.com/mhahsler/dbscan/issues/56

--- analysis/clustering.R
+++ analysis/clustering.R
@@ -1,6 +1,7 @@
 library(cowplot)
 library(dbscan)
 library(dplyr)
+library(ggExtra)
 library(ggforce)
 library(ggplot2)
 library(ggrepel)
@@ -8,6 +9,9 @@ library(purrr)
 library(readxl)
 library(stringr)
 library(tidyr)
+
+
+# DATA --------------------------------------------------------------------
 
 varna <- read_xlsx("data/varna_human_isotopes.xlsx",
                    sheet = 1,
@@ -25,16 +29,21 @@ varna <- read_xlsx("data/varna_human_isotopes.xlsx",
   ) |>
   distinct(grave, .keep_all = TRUE) # remove replicates
 
+
+# PLOT DISTRIBUTION -------------------------------------------------------
+
 # Scatter plot
-ggplot(varna, aes(d13c, d15n, label = grave)) +
+fig_varna_dist <- ggplot(varna, aes(d13c, d15n, label = grave)) +
   geom_point() +
   geom_text_repel(colour = "darkgrey") +
   scale_x_reverse() +
   labs(x = "δ13C", y = "δ15N") +
   theme_cowplot()
 
+ggMarginal(fig_varna_dist, type = "boxplot", size = 10)
+
 # Scatter plot with lab error envelopes
-varna |>
+fig_varna_dist_error <- varna |>
   mutate(
     d13c_min = d13c - 0.2,
     d13c_max = d13c + 0.2,
@@ -57,7 +66,10 @@ varna |>
   labs(x = "δ13C", y = "δ15N") +
   theme_cowplot()
 
-# DBSCAN clustering
+ggMarginal(fig_varna_dist_error, type = "boxplot", size = 10)
+
+# DBSCAN ------------------------------------------------------------------
+
 dbscan_min_points <- 3 # Three's a crowd
 
 # Select optimal eps from KNN plot
@@ -82,7 +94,7 @@ varna |>
   varna_cluster
 
 # Plot clusters
-ggplot(varna_cluster, aes(d13c, d15n, shape = site, group = cluster)) +
+fig_varna_cluster <- ggplot(varna_cluster, aes(d13c, d15n, shape = site, group = cluster)) +
   geom_point() +
   geom_mark_hull(aes(fill = cluster, colour = cluster), na.rm = TRUE) +
   #geom_text_repel(aes(label = grave), colour = "darkgrey") +
@@ -90,4 +102,43 @@ ggplot(varna_cluster, aes(d13c, d15n, shape = site, group = cluster)) +
   scale_colour_brewer(palette = "Set1", guide = guide_none()) +
   scale_fill_brewer(palette = "Set1", guide = guide_none()) +
   labs(x = "δ13C", y = "δ15N", shape = NULL) +
-  theme_cowplot()
+  theme_cowplot() +
+  theme(legend.position = "bottom")
+
+ggMarginal(fig_varna_cluster, type = "boxplot", size = 10)
+
+
+# HDBSCAN -----------------------------------------------------------------
+varna |>
+  nest() |>
+  mutate(
+    cluster_data = map(data, select, d13c, d15n),
+    hdbscan = map(cluster_data, hdbscan, minPts = dbscan_min_points),
+    cluster = map(hdbscan, "cluster"),
+    cluster_p = map(hdbscan, "membership_prob")
+  ) |>
+  select(data, cluster, cluster_p) |>
+  unnest(c(data, cluster, cluster_p)) |>
+  mutate(
+    cluster = na_if(cluster, 0),
+    cluster = factor(cluster)
+  ) ->
+  varna_hcluster
+
+# TODO: suppress clusters where eps is less than the measurement error
+# Method after Malzer & Baum 2019 (https://arxiv.org/abs/1911.02282)
+# Depends on: https://github.com/mhahsler/dbscan/issues/56
+
+fig_varna_hcluster <- ggplot(varna_hcluster,
+                             aes(d13c, d15n, group = cluster)) +
+  geom_point(aes(shape = site)) +
+  geom_mark_hull(aes(fill = cluster, colour = cluster), na.rm = TRUE) +
+  #geom_text_repel(aes(label = grave), colour = "darkgrey") +
+  scale_x_reverse() +
+  scale_colour_brewer(palette = "Set1", guide = guide_none()) +
+  scale_fill_brewer(palette = "Set1", guide = guide_none()) +
+  labs(x = "δ13C", y = "δ15N", shape = NULL) +
+  theme_cowplot() +
+  theme(legend.position = "bottom")
+
+ggMarginal(fig_varna_hcluster, type = "boxplot", size = 10)
